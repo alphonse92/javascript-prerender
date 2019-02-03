db4d85cf07feb9a4a5f4e8942070243ecb073805
Alejandro Molina <alejandromolina@kubesoft.com>
2019-02-03 15:10:53 -0500

    adding the correct redisCLient context

--- prerender/app/lib/cache/systems/redis.js
+++ prerender/app/lib/cache/systems/redis.js
@@ -9,7 +9,7 @@ function createClient(conf) {
     get: null,
     set: null,
   }
-  const createSchema = (client, key) => Object.assign(client, { [key]: util.promisify(redisClient[key].bind(client)) })
+  const createSchema = (client, key) => Object.assign(client, { [key]: util.promisify(redisClient[key].bind(redisClient)) })
   return Object.keys(schema).reduce(createSchema, schema)
 }
 function getOrClientClient(conf) {
