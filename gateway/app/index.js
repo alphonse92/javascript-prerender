73ec34221b7de0a0ea89d626ee82fd9abf0ad410
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-02-12 12:03:40 -0500

    installing some utils libraries and starting to expose endpoints, missing another tasks

--- gateway/app/index.js
+++ gateway/app/index.js
@@ -1,15 +1,20 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import config from '../config'
-import middleware_builder_fn from './prerender'
+import controller from './controller'
 
 export default async function initApp() {
 	const app = express();
-	const prerender_middleware = await middleware_builder_fn()
-	app.use("*", prerender_middleware)
+	app.use(bodyParser.urlencoded({ extended: false }))
+	app.use(bodyParser.json())
+	app.use("/health", controller.health)
+	app.use("/", controller.health)
+	app.use('*', controller.proxy)
 	app.disable('x-powered-by')
 	const server = app.listen(config.port, function () {
 		var port = server.address().port;
-		console.log('Server is running on port:', port);
-		return server; 
+		console.log('Gateway is runing', port);
+		return server;
 	});
 };
