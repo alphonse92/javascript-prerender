0b16599ec7061c044bb66a267397f99a2db83644
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-02-18 16:19:07 -0500

    adding script to update the ms-lib and the microservices that uses it

---
+++ publish_libraries.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Parameters
+# $1 is the commit message
+
+function installAndUpdate(){
+  local MICROSERVICE=${1}
+  local uri=$(cat $MICROSERVICE/package.json | jq ".dependencies[\"@alphonse92/ms-lib\"]")
+    
+  if [ $uri != "null" ]; then
+    echo " == > updating @alphonse92/ms-lib"
+    cd $MICROSERVICE
+    npm update @alphonse92/ms-lib
+    cd ..
+  fi
+
+}
+
+#Setting microservices folders
+MICROSERVICES=(
+  "./gateway"
+  "./prerender"
+)
+
+## STARTING GIT TASK
+
+  ## setting a default commit message
+  COMMIT_MSG=${1}
+  if [ -z "$COMMIT_MSG" ] ; then
+    COMMIT_MSG="updating library"
+  fi;
+
+  git add ./microservice-lib
+  git commit -m "$1"
+## END GIT  TASK
+
+# STARTING TO UPDATE NPM PACKAGES
+for microservice in "${MICROSERVICES[@]}"; do ( echo && echo "updating common lib $microservice" &&  installAndUpdate "${microservice}" && echo ) done
+# END OF UPDATE NPM PACKAGES
