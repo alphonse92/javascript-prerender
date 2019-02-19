9495c877f7761c758564907662832c4b53fd670b
Eliecer Alejandro Molina Vergel <eliecer.molina@uruit.com>
2019-02-19 10:43:27 -0500

    use config creator from ms-lib instead of local implementationb

--- gateway/config/index.js
+++ gateway/config/index.js
@@ -1,20 +1,12 @@
 import _ from 'lodash'
-
-const confSchema = {
-	config: {
-		env: process.env.NODE_ENV || 'local',
-	}
-}
-
-const confFromEnv = Object
-	.keys(process.env)
-	.reduce((out, key) => _.set(out, key.replace(new RegExp('_', 'g'), '.'), process.env[key]), {})
-const env = confSchema.config.env
-const confFromFileConf = require('./env/' + env.toLowerCase()).default
-const mergedConf = _.merge(_.merge(confSchema, confFromFileConf), confFromEnv)
-
+import { Config } from '@alphonse92/ms-lib'
+const env = process.env.NODE_ENV
+const fileConfig = require('./env/' + env.toLowerCase()).default
+const schema = { config: { env: env || 'local', } }
+const mergedConf = Config.create(schema, process.env, fileConfig)
 const { config } = mergedConf
 config.blacklist = config.blacklist ? config.blacklist.split('|') : []
 config.useragents.redirect = config.useragents.redirect ? config.useragents.redirect.split('|') : []
 config.useragents.block = config.useragents.block ? config.useragents.block.split('|') : []
+
 export default config
